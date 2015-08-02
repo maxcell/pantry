6b75458cb527d8fa7ed1828cb4f8a39531eb4ec8
Maxcell Wilson <maxcell.wilson@gmail.com>
2015-08-01 21:09:02 -0400

    Successfully parse JSON and add categories

--- app/src/main/java/com/example/android/materialtest/CardListFragment.java
+++ app/src/main/java/com/example/android/materialtest/CardListFragment.java
@@ -4,12 +4,22 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
 /**
  * Created by Prince on 7/28/15.
@@ -46,15 +56,21 @@ public class CardListFragment extends Fragment {
 //        Not sure how I feel about this one yet
 //        categories.add("Baby Items");
 
-        categories.add("Bakery");
-        categories.add("Beverages");
-        categories.add("Cereal");
-        categories.add("Deli");
-        categories.add("Fruits");
-        categories.add("Vegetables");
-        categories.add("Meat");
-        categories.add("Grains");
-        categories.add("Dairy");
+//        categories.add("Bakery");
+//        categories.add("Beverages");
+//        categories.add("Cereal");
+//        categories.add("Deli");
+//        categories.add("Fruits");
+//        categories.add("Vegetables");
+//        categories.add("Meat");
+//        categories.add("Grains");
+//        categories.add("Dairy");
+
+        HashMap<String, ArrayList<String>> category = parseJSON();
+        for(int i = 0; i < category.keySet().size(); i++){
+            categories.add((String) (category.keySet().toArray())[i]);
+        }
+
 
         // Sort the contents
         Collections.sort(categories);
@@ -62,6 +78,61 @@ public class CardListFragment extends Fragment {
         // Add this last to keep any user added items out of the way
         categories.add("Miscellaneous");
 
+//        Log.v("INSIDE", (String) (category.keySet().toArray())[0]);
+
         return categories;
+    }
+
+
+    private String readJSON(){
+
+        String json;
+        try{
+            InputStream is = getActivity().getAssets().open("grocerydata.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch(IOException ex){
+            ex.printStackTrace();
+            return null;
+        }
+
+        return json;
+    }
+
+    private HashMap<String, ArrayList<String>> parseJSON(){
+        HashMap<String, ArrayList<String>> categories = new HashMap<>();
+        try{
+            JSONObject obj = new JSONObject(readJSON());
+            JSONArray mJSONArray = obj.getJSONArray("groceries");
+
+
+
+
+            for(int i = 0; i < mJSONArray.length(); i++){
+                ArrayList<String> itemList = new ArrayList<>();
+                JSONObject inArray = mJSONArray.getJSONObject(i);
+                String title = inArray.getString("category");
+
+                JSONArray categoryList = inArray.getJSONArray("items");
+
+                for(int j = 0; j < categoryList.length(); j++){
+                itemList.add(categoryList.getString(j));
+                }
+
+                categories.put(title, itemList);
+            }
+
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
+
+        finally {
+            return categories;
+        }
+
     }
 }
